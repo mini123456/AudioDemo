41bb253df88a123d403d3890d50c4b1fd3b7546d
tusm <tusm@tusmdeMacBook-Pro.local>
2015-02-14 16:06:30 +0800

    Initial Commit

---
+++ AudioDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioDemo
+//
+//  Created by tusm on 15/2/14.
+//  Copyright (c) 2015年 tusm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AudioDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AudioDemo
+//
+//  Created by tusm on 15/2/14.
+//  Copyright (c) 2015年 tusm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
